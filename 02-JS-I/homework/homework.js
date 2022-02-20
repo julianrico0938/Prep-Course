d270818a23f363618f2ba3a76f5d3c1c3b76041b
julianrico0938 <jrico0938@gmail.com>
2022-02-20 00:31:21 -0500

    Avance 19/02/2022

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -256,12 +256,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
-  if(letra.equalsIgnoreCase = 'a' || 'e' || 'i' || 'o' || 'u'){
-    return "Es vocal";
-  }else if(letra.length){
-  return "Dato incorrecto";
-  }
+
 }
 // No modificar nada debajo de esta línea
 // --------------------------------
